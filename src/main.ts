ec0ca735daa475233a82887d998ddb10c634ee71
giathuan0310 <luonggiathuan2019.gr@gmail.com>
2025-08-22 20:50:09 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const port = process.env.PORT || 3000;
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
   });
-  await app.listen(3000,'0.0.0.0');
+
+  await app.listen(port, '0.0.0.0');
 
 }
 bootstrap();
